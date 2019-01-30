a6f1daaf4fac6c9ab96151dafcd3f0af5b87ae11
Grant Iraci <grantira@buffalo.edu>
2019-01-30 11:23:25 -0500

    Add TX stress test script

---
+++ util/tx_stress.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3.7
+
+import sys
+import numpy
+import time
+from com_radio import Radio
+
+if __name__ == "__main__":
+
+    radio = Radio(sys.argv[1])    
+    num_pkts = int(sys.argv[2])
+    delay_s = int(sys.argv[3]) / 1000.0
+
+    radio.reset()
+    time.sleep(0.5)
+
+    for i in range(num_pkts):
+        print('Sending packet {:03d}...'.format(i))
+        radio.tx('TEST!' * 25)
+        time.sleep(delay_s)
+    
+
